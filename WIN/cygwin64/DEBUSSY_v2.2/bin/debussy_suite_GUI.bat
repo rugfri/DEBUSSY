179cba76d256d17ff2efc433455ee005b118cde8
Federica Bertolotti <federica.bertolotti@uninsubria.it>
2021-09-04 10:26:43 +0200

    Distributed Sept 2021

--- WIN/cygwin64/DEBUSSY_v2.2/bin/debussy_suite_GUI.bat
+++ WIN/cygwin64/DEBUSSY_v2.2/bin/debussy_suite_GUI.bat
@@ -1,1 +1,2 @@
-python.exe C:\cygwin64\DEBUSSY_v2.2\GUI\DebUsSy-Suite_GUI.py
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
+python.exe C:\cygwin64\DEBUSSY_v2.2_2019\GUI\DebUsSy-Suite_GUI.py
--- WIN/debussy_suite_GUI_anaconda_prompt.bat
+++
@@ -1,2 +0,0 @@
-call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
-python.exe C:\cygwin64\DEBUSSY_v2.2_2019\GUI\DebUsSy-Suite_GUI.py
